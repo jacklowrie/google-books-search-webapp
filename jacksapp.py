b6019d689beb0592833d67c1423115df61e39142
jacklowrie <jack@4thwall.io>
2019-01-27 15:22:51 -0600

    test accessing response dictionary, make sure search is properly urlencoded

--- jacksapp.py
+++ jacksapp.py
@@ -1,13 +1,16 @@
 
 
-import requests
+import requests #this thing is awesome
 
 #get query from frontend
     #...code goes here
 #parse query
     #...code goes here
 #construct the GET request
-parameters = {'q' : 'amulet+of+samarkand', 'projection' : 'lite'}
+hp = 'harry potter sorcerer\'s stone' #test search many results
+quarterboys = 'intitle:reckoning+inauthor:david+inauthor:lennon' #test search one result
+noresults = '3ugn398' #test search no results
+parameters = {'q' : noresults, 'projection' : 'lite'}
 
 #send to google books
 search = requests.get('https://www.googleapis.com/books/v1/volumes', params=parameters)
@@ -15,6 +18,8 @@ search = requests.get('https://www.googleapis.com/books/v1/volumes', params=para
 #parse the results
 results = search.json()
 
-#format for frontend
-#display
+#format for frontend display
+print('number of results: ' + str(results.get("totalItems")) + '\n\n\n')
+print(search.url + '\n\n\n')
+print(str(search.status_code) + '\n\n\n')
 print(results)
