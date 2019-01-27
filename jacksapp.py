dd779073c3275f8ed59bd3fb3de3c8ed0e23c732
jacklowrie <jack@4thwall.io>
2019-01-27 14:40:37 -0600

    break request into parameters

--- jacksapp.py
+++ jacksapp.py
@@ -3,11 +3,14 @@
 import requests
 
 #get query from frontend
-
+    #...code goes here
 #parse query
+    #...code goes here
+#construct the GET request
+parameters = {'q' : 'amulet+of+samarkand', 'projection' : 'lite'}
 
 #send to google books
-search = requests.get("https://www.googleapis.com/books/v1/volumes?q='the+amulet+of+samarkand'")
+search = requests.get('https://www.googleapis.com/books/v1/volumes', params=parameters)
 
 #parse the results
 results = search.json()
