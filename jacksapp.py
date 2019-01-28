e818a5c440baac23dbb426500ce01a992e72f358
jacklowrie <jack@4thwall.io>
2019-01-27 20:00:37 -0600

    start OOPing. result parsing works for one result and one author

--- jacksapp.py
+++ jacksapp.py
@@ -2,24 +2,58 @@
 
 import requests #this thing is awesome
 
-#get query from frontend
-    #...code goes here
-#parse query
-    #...code goes here
-#construct the GET request
+# test queries
 hp = 'harry potter sorcerer\'s stone' #test search many results
 quarterboys = 'intitle:reckoning+inauthor:david+inauthor:lennon' #test search one result
-noresults = '3ugn398' #test search no results
-parameters = {'q' : noresults, 'projection' : 'lite'}
+noresults = '3ugn398' #test search returns no results
 
-#send to google books
-search = requests.get('https://www.googleapis.com/books/v1/volumes', params=parameters)
+class BookSearch:
 
-#parse the results
-results = search.json()
+    books_api = 'https://www.googleapis.com/books/v1/volumes'
+    parameters = {'q' : noresults, 'projection' : 'lite'}
+    search = ''
+    results = ''
 
-#format for frontend display
-print('number of results: ' + str(results.get("totalItems")) + '\n\n\n')
-print(search.url + '\n\n\n')
-print(str(search.status_code) + '\n\n\n')
-print(results)
+    def __init__(self, search):
+        self.search = search
+
+#construct the GET request
+    def construct_request(self):
+        self.parameters['q'] = self.search
+
+    def send_request(self):
+        self.search = requests.get(self.books_api, params=self.parameters)
+
+    def parse_results(self):
+        self.results = self.search.json()
+
+    def make_a_search(self):
+        self.construct_request()
+        self.send_request()
+        self.parse_results()
+
+
+
+
+    def print_search_url(self):
+        print(self.search.url)
+    def print_search_status_code(self):
+        print('HTTP Status Code: ' + str(self.search.status_code))
+    def print_results_count(self):
+        print('number of results: ' + str(self.results.get("totalItems")))
+    def print_results(self):
+        print(self.results)
+    def print_result_title(self):
+        print ('title: ' + self.results['items'][0]['volumeInfo']['title']
+                +': '
+                + self.results['items'][0]['volumeInfo']['subtitle']
+                + '\n\t'
+                + str(self.results['items'][0]['volumeInfo']['authors']))
+    def dump_search_results(self):
+        self.print_search_status_code()
+        self.print_results_count()
+        self.print_result_title()
+#try it out
+test = BookSearch(quarterboys)
+test.make_a_search()
+test.dump_search_results()
