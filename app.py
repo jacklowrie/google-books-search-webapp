de8a309251e3043e233be42132048f7c8210f9ce
jacklowrie <jack@4thwall.io>
2019-01-28 07:18:08 -0600

    configuring for heroku

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 from flask import Flask, render_template, request
 from form import SearchForm
 from booksearch import BookSearch
+import logging
+import sys
 
 app = Flask(__name__)
 app.secret_key = 'QVwI5uhPuU'
+
+app.logger.addHandler(logging.StreamHandler(sys.stdout))
+app.logger.setLevel(logging.ERROR)
 
 @app.route('/', methods=["GET", "POST"])
 def home():
@@ -15,11 +20,11 @@ def home():
         else:
             # Query google books
             search_query = form.search_query.data
-            book_search = BookSearch(search_query) #
+            book_search = BookSearch(search_query)
             results = book_search.get_search_results()
             return render_template("index.html", form=form, results=results)
     elif request.method == 'GET': #if returning results
         return render_template("index.html", form=form, results=results)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
