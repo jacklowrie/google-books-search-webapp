0ede7df6ad33ac62dc311ea8664cafcd0cf1e8b5
jacklowrie <jack@4thwall.io>
2019-01-27 20:49:42 -0600

    Can now get author and title, get_title() properly handles subtitles

--- jacksapp.py
+++ jacksapp.py
@@ -2,28 +2,26 @@
 
 import requests #this thing is awesome
 
-# test queries
-hp = 'harry potter sorcerer\'s stone' #test search many results
-quarterboys = 'intitle:reckoning+inauthor:david+inauthor:lennon' #test search one result
-noresults = '3ugn398' #test search returns no results
 
 class BookSearch:
 
     books_api = 'https://www.googleapis.com/books/v1/volumes'
-    parameters = {'q' : noresults, 'projection' : 'lite'}
+    parameters = {'q' : '', 'projection' : 'lite'}
     search = ''
     results = ''
 
     def __init__(self, search):
         self.search = search
 
-#construct the GET request
+    #construct the GET request
     def construct_request(self):
         self.parameters['q'] = self.search
 
+    #send the GET request
     def send_request(self):
         self.search = requests.get(self.books_api, params=self.parameters)
 
+    #store the results in a python dictionary
     def parse_results(self):
         self.results = self.search.json()
 
@@ -32,28 +30,47 @@ class BookSearch:
         self.send_request()
         self.parse_results()
 
-
-
-
-    def print_search_url(self):
+    def print_search_url(self): #just for debugging
         print(self.search.url)
-    def print_search_status_code(self):
-        print('HTTP Status Code: ' + str(self.search.status_code))
+
+    # returns the status code as an int
+    def get_status_code(self):
+        return self.search.status_code
+
+    # returns the total items found as an int
     def print_results_count(self):
-        print('number of results: ' + str(self.results.get("totalItems")))
+        return self.results.get("totalItems")
+
     def print_results(self):
         print(self.results)
-    def print_result_title(self):
-        print ('title: ' + self.results['items'][0]['volumeInfo']['title']
-                +': '
-                + self.results['items'][0]['volumeInfo']['subtitle']
-                + '\n\t'
-                + str(self.results['items'][0]['volumeInfo']['authors']))
+
+    def get_result_title(self):
+        title = self.results['items'][0]['volumeInfo']['title']
+
+        if 'subtitle' in self.results['items'][0]['volumeInfo']:
+            title += ': ' + self.results['items'][0]['volumeInfo']['subtitle']
+
+        return 'title: ' + title
+
+    def get_result_author(self):
+        authors = ', '.join(self.results['items'][0]['volumeInfo']['authors'])
+        return 'authors: ' + authors
+
     def dump_search_results(self):
-        self.print_search_status_code()
-        self.print_results_count()
-        self.print_result_title()
+        print("HTTP Status Code: " +str(self.get_status_code()))
+        print("total results: " + str(self.print_results_count())+'\n')
+        print(self.get_result_title())
+        print('\t' + self.get_result_author())
+
+
+# test queries
+many_results = 'harry potter sorcerer\'s stone' #test search many results
+quarter_boys = 'intitle:reckoning+inauthor:david+inauthor:lennon' #test search one result
+no_results = '3ugn398' #test search returns no results
+multiple_authors = 'introduction to algorithms inauthor:Thomas inauthor:H inauthor:Cormen inauthor:Thomas inauthor:H inauthor:Cormen inauthor:Charles inauthor:E inauthor:Leiserson'
+
+
 #try it out
-test = BookSearch(quarterboys)
+test = BookSearch(multiple_authors)
 test.make_a_search()
 test.dump_search_results()
