58e7ccba563caeea6c3286ba5c9de2cc1f17726f
jacklowrie <jack@4thwall.io>
2019-03-08 15:11:42 -0600

    began writing APIQuery class

--- apiquery.py
+++ apiquery.py
@@ -2,7 +2,14 @@ import requests
 
 class APIQuery:
     base_url = ''
-    parameters = []
+    parameters = {'q':''}
 
-    def __init__(self, base_url):
+    def __init__(self, base_url, search):
         self.base_url = base_url
+        self.parameters['q'] = search
+
+    def get_base_url(self):
+        return self.base_url
+
+    def get_parameters(self):
+        return self.parameters
--- test_apiquery.py
+++ test_apiquery.py
@@ -4,7 +4,31 @@ from unittest.mock import Mock, patch
 from apiquery import APIQuery
 
 # test can set base base_url
-def test_can_create_apiquery():
+def test_can_create_apiquery_with_base_url():
     test_url = 'https://www.example.com/'
-    new_apiquery = APIQuery(test_url)
+    new_apiquery = APIQuery(test_url, '')
     assert new_apiquery.base_url == test_url
+
+def test_can_create_apiquery_with_params():
+    test_url = 'https://www.example.com/'
+    test_query = 'some query'
+    new_apiquery = APIQuery(test_url, test_query)
+
+    assert new_apiquery.parameters['q'] == test_query
+
+def test_can_get_base_url():
+    test_url = 'https://www.example.com/'
+    test_query = 'some query'
+    new_apiquery = APIQuery(test_url, test_query)
+
+    assert new_apiquery.get_base_url() == test_url
+
+def test_can_get_parameters():
+    test_url = 'https://www.example.com/'
+    test_query = 'some query'
+    new_apiquery = APIQuery(test_url, test_query)
+
+    assert new_apiquery.get_parameters() == {'q': test_query}
+
+def test_can_send_request():
+    assert False
